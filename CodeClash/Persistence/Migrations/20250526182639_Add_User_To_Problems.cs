df2ac1c271e8f70f1858097f6e5e313c8c28172b
Гурин Артур <a.gurin@bars.group>
2025-05-26 21:28:29 +0300

    feat/problems-crud: migration

---
+++ CodeClash/Persistence/Migrations/20250526182639_Add_User_To_Problems.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class Add_User_To_Problems : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<long>(
+                name: "UserId",
+                table: "Problems",
+                type: "bigint",
+                nullable: false,
+                defaultValue: 0L);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Problems_Name",
+                table: "Problems",
+                column: "Name",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Problems_UserId",
+                table: "Problems",
+                column: "UserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Problems_Users_UserId",
+                table: "Problems",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Problems_Users_UserId",
+                table: "Problems");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Problems_Name",
+                table: "Problems");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Problems_UserId",
+                table: "Problems");
+
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                table: "Problems");
+        }
+    }
+}
